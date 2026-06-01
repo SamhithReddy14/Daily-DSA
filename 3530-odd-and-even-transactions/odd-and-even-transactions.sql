df6ad6c6f36503e1cc6075eba37628818bddc07f
Samhith Reddy <163881853+SamhithReddy14@users.noreply.github.com>
2026-06-01 14:53:00 +0530

    Time: 294 ms (59.64%) | Memory: 0B (100.00%) - LeetSync

--- 3530-odd-and-even-transactions/odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -2,7 +2,8 @@
 select transaction_date , sum(
     case when mod(amount,2)=1 then amount else 0 end
 ) as odd_sum ,
-sum(case when mod(amount,2) = 0 then amount else 0 end) as even_sum
+sum(case when mod(amount,2) = 0 then amount else 0 end
+) as even_sum
 from transactions 
 group by transaction_date 
 order by transaction_date ;
