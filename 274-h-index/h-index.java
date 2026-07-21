a1a11a5b7b00628c065c59e2192b10a4a9519403
Samhith Reddy <163881853+SamhithReddy14@users.noreply.github.com>
2026-07-21 14:00:31 +0530

    Time: 6 ms (29.02%) | Memory: 43.5 MB (44.11%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        
+        for (int i = 0; i < n; i++) {
+            int papersRemaining = n - i;
+            if (citations[i] >= papersRemaining) {
+                return papersRemaining;
+            }
+        }
+        
+        return 0;
+    }
+}
