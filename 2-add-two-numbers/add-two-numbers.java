0b13c8cd5c9e24ca630bdd061fb6453dc3415da3
Samhith Reddy <163881853+SamhithReddy14@users.noreply.github.com>
2026-09-15 22:58:46 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (77.11%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,46 +1,22 @@
-import java.math.BigInteger;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-
-        while (l1 != null) {
-            sb1.append(l1.val);
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            sb2.append(l2.val);
-            l2 = l2.next;
-        }
-
-        BigInteger num1 = new BigInteger(sb1.reverse().toString());
-        BigInteger num2 = new BigInteger(sb2.reverse().toString());
-
-        BigInteger sum = num1.add(num2);
-
-        String s = new StringBuilder(sum.toString()).reverse().toString();
-
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
-        for (int i = 0; i < s.length(); i++) {
-            curr.next = new ListNode(s.charAt(i) - '0');
-            curr = curr.next;
-        }
-
-        return dummy.next;
+        ListNode dummy = new ListNode(-1) ;
+        ListNode tail = dummy ;
+        int carry = 0 ;
+         while(l1!=null ||l2!= null || carry!=0) {
+            int digit1 = (l1!=null) ? l1.val : 0 ;
+            int digit2 = (l2!=null) ? l2.val : 0 ;
+            int sum = digit1+digit2+carry ;
+            int digit = sum %10 ;
+            carry = sum / 10 ;
+            ListNode newNode = new ListNode(digit) ;
+            tail.next = newNode ;
+            tail = tail.next ;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+         }
+         ListNode result = dummy.next;
+        dummy.next = null;
+        return result;
     }
 }
